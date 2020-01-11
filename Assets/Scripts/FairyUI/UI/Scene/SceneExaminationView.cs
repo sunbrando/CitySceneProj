23356bbbc2ce4540f101be842e51ad44627bc3f7
sunbrando <k_xun@qq.com>
2020-01-11 23:56:56 +0800

    优化

--- Assets/Scripts/FairyUI/UI/Scene/SceneExaminationView.cs
+++ Assets/Scripts/FairyUI/UI/Scene/SceneExaminationView.cs
@@ -32,7 +32,7 @@ public class SceneExaminationView : SceneView
         GComponent com_title = uIBackground.GetChild("com_title").asCom;
         GTextField text = com_title.GetChild("title").asTextField;
         text.text = @"      一名參加駕照考試的市民，於應考時將一千澳門元塞入考官手中，意圖行賄考官以順利通過駕駛考試，考官即時拒絕及舉報廉政公署。案件由廉政公署移交檢察院偵辦。
-法院判處該名市民行賄罪名成立，處7個月徒刑，緩刑1年6個月。";
+     法院判處該名市民行賄罪名成立，處7個月徒刑，緩刑1年6個月。";
         PlayTypeEffect(text);
 
         button_Close.onClick.Set(() => {
